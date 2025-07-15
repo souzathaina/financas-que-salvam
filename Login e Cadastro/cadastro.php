e795da23fa92c9a8f8eabbf264d4f2788e5e105a
Pedro Algayer <126249729+PedroSenac123@users.noreply.github.com>
2025-07-14 21:53:44 -0300

    Login e Cadastro

---
+++ Login e Cadastro/cadastro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cadastro</title>
+  <link rel="stylesheet" href="cadastro.css">
+</head>
+<body>
+  <div class="container">
+    <div class="cadastro-box">
+      <h2>Cadastro</h2>
+      <form>
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" placeholder="Campo de texto">
+
+        <label for="telefone">Telefone:</label>
+        <input type="text" id="telefone" placeholder="Campo de texto">
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" placeholder="Campo de texto">
+
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" placeholder="Campo de texto">
+
+        <button type="submit">Cadastre-se</button>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
---
+++ Login e Cadastro/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="login.css">
+</head>
+<body>
+  <div class="container">
+    <div class="login-box">
+      <h2>Login</h2>
+      <form>
+        <label for="email">Email:</label>
+        <input type="text" id="email" placeholder="Campo de texto">
+
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" placeholder="Campo de texto">
+
+        <button type="submit">Login</button>
+      </form>
+      <p class="register-text">Não tem uma conta? <a href="#">Registre-se aqui</a></p>
+    </div>
+  </div>
+</body>
+</html>
